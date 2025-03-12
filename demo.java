cb7b37ab0b69f19352c59d8e13c30f29220dbbb0
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-12 15:31:41 +0530

    hpy appy

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo{
+    public static void main(String[] args) {
+        String name="niha";
+        String name2="krish";
+        System.out.println(name+name2);
+    }
+}
+
+    
