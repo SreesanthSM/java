93cf0e3b73793328930f2e742f2a3f195b8c69f9
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-13 14:44:06 +0530

    if else

---
+++ con1.java
@@ -0,0 +1,12 @@
+public class con1 {
+    public static void main(String[] args) {
+    int age=45;
+        if(age>=18){
+            System.out.println("he is major");
+
+        }
+        else{
+            System.out.println(".(he is minor");
+        }
+    }
+}
