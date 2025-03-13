27e7210caec223fba6b0723f4a3ce77efcc6c846
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-13 11:35:36 +0530

    addition

---
+++ maths.java
@@ -0,0 +1,10 @@
+public class maths{
+    public static void main(String[] args) {
+        int x=5;
+        int y=9;
+        int a=51;
+        int b=12;
+        System.out.println(a*b);
+        System.out.println(x+y);
+    }
+}
