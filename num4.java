c08f72d3ba2e4e65f262a2eef8f33abe3559fa0e
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-13 12:03:24 +0530

    logical

---
+++ num4.java
@@ -0,0 +1,8 @@
+public class num4 {
+    public static void main(String[] args) {
+        int a=23;
+        int b=56;
+        System.out.println(a!=b&&a>b);
+        System.out.println(a==b||a<b);
+    }
+}
