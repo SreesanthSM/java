6b4414bafaa379961c61c0bbb3787fd8f6961cfd
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-13 09:58:52 +0530

    about class

---
+++ mark.java
@@ -0,0 +1,13 @@
+
+public class mark {
+    public static void main(String[] args) {
+        String Name="sreesanth";
+        System.out.println(name);
+        String dept="cse-b";
+        System.out.println(dept);
+        int mark=87;
+        System.out.println(mark);
+        Char grade=b+;
+        System.out.println(grade);
+    }
+}
