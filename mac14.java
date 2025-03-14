865677aaaee49335dfb72ab9fddd11d58472c77b
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-14 13:54:24 +0530

    hjduer

---
+++ mac14.java
@@ -0,0 +1,6 @@
+public class mac14 {
+    public static void main(String[] args) {
+        String [] names={"abhi","niha","krish","sri","mac","sena"};
+        System.out.println(names[2]);
+    }
+}
