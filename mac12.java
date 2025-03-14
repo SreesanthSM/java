f8099721783ba30febafb11149f86ead15332279
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-14 13:23:47 +0530

    dhgfy

---
+++ mac12.java
@@ -0,0 +1,13 @@
+public class mac12 {
+
+        static void sri(){
+            int a=13;
+            int b=2;
+            System.out.println(a*b);
+        }
+        public static void main(String[] args) {
+            sri();
+        }  
+        }
+    
+
