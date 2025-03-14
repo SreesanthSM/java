fb6fd99bd76241f1a86c4c6b3c55acda453f2e89
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-14 14:04:12 +0530

    da bhai

---
+++ mac15.java
@@ -0,0 +1,10 @@
+public class mac15 {
+        public static void main(String[] args) {
+            String [] names={"abhi","niha","krish","sri","mac","sena"};
+            System.out.println(names[2]);
+            System.out.println(names[3]);
+            System.out.println(names.length);
+        }
+    }
+    
+
