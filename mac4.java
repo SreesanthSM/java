3f3f85eaebbcbfe1af36853c9ee9d943e3b7350b
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-14 10:55:25 +0530

    do while

---
+++ mac4.java
@@ -0,0 +1,12 @@
+public class mac4 {
+    public static void main(String[] args) {
+        int i=0,n=5;
+        do{
+            System.out.println(n);
+            i=i+1;
+        }while (i<5);
+           
+        }
+    }
+
+
---
+++ num78.java
@@ -0,0 +1,16 @@
+public class num78{
+    public static void main(String[] args){
+        int day=2;
+        Switch (day) {
+        case 1:
+            System.out.println("monday");
+        break;
+        case 2:
+             System.out.println("tuesday");
+        break;
+        default:
+            System.out.println("there are only 7 days in a week !");
+    }
+}
+    
+}
