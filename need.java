9e1fd07eba6ccecec2852b6ae97558d12ab1c5fa
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-15 10:29:44 +0530

    gfhfghe

--- need.java
+++ need.java
@@ -2,7 +2,7 @@ public class need {
     public static void main(String[] args) {
         int i;
         int [] a={101,102,103,104,105};
-        for(i=0;i<=5;i++){
+        for(i=0;i<5;i++){
             System.out.println(a[i]);
     }
 }
