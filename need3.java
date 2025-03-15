f752eac17d0806de9b6a9abd15761753c5c6e564
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-15 11:30:36 +0530

    g324

---
+++ need3.java
@@ -0,0 +1,21 @@
+public class need3 {
+    public static void main(String[] args) {
+        int menu=1;
+        switch (menu){
+            case 1:
+            System.out.println("coffee");
+            break;
+            case 2:
+            System.out.println("tea");
+            break;
+            case 3:
+            System.out.println("ginger tea");
+            break;
+            default:
+            System.out.println("not available");
+
+
+        }
+        }
+    }
+
