cc910a7ceb6ed43742f80292b46f65ba86ed809f
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-15 10:26:41 +0530

    done

---
+++ need.java
@@ -0,0 +1,9 @@
+public class need {
+    public static void main(String[] args) {
+        int i;
+        int [] a={101,102,103,104,105};
+        for(i=0;i<=5;i++){
+            System.out.println(a[i]);
+    }
+}
+}
