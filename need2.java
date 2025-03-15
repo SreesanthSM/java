8458fee49b1732ed05a8d1c83cc278ba3fa491bf
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-15 11:01:55 +0530

    gfhfgg

---
+++ need2.java
@@ -0,0 +1,11 @@
+public class need2 {
+    public static void main(String[] args) {
+        int i;
+        String [] a={"apple","orange","pine","mango","grapes"};
+        int [] b={23,54,67,45,21};
+        for(i=0;i<5;i++){
+        System.out.println(a[i]+" \t:"+b[i]);
+
+    }
+}
+}
