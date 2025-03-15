b511731aa224d2dad0645eb8bca12615802f9932
SreesanthSM <sreesanth543ty@gmail.com>
2025-03-15 12:13:10 +0530

    g3hghgfg4

---
+++ need4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class need4 {
+    public static void main(String[] args) {
+        Scanner o1=new Scanner(System.in);
+        System.out.println("enter the user name");
+        String username=o1.nextLine();
+        System.out.println("username:"+username);
+    }
+}
